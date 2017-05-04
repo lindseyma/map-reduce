60da0eb75e588b8beb93fba17a44453781b763e0
lindseyma <lma2@stuy.edu>
2017-05-03 22:56:28 -0400

    change

--- reduce.py
+++ reduce.py
@@ -12,8 +12,11 @@ print single("Lizzy")
 ##find the total frequency of a group of words
 def group(words):
 	##create list of result for frequency of each word
-	frequencyList = [single(word) for word in words]
+	frequencyList = map(single, words)
 	##use reduce to add each frequency in list
 	return reduce(lambda a,b: a+b, frequencyList)
 
 print group(["the", "Lizzy"])
+
+##most frequently occuring word
+##def most():
