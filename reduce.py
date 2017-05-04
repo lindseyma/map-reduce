06dc6491ec3f79ee695383edc88fd08fd1df2486
lindseyma <lma2@stuy.edu>
2017-05-03 22:36:50 -0400

    group

--- reduce.py
+++ reduce.py
@@ -8,3 +8,12 @@ def single(word):
 
 print single("the")
 print single("Lizzy")
+
+##find the total frequency of a group of words
+def group(words):
+	##create list of result for frequency of each word
+	frequencyList = [single(word) for word in words]
+	##use reduce to add each frequency in list
+	return reduce(lambda a,b: a+b, frequencyList)
+
+print group(["the", "Lizzy"])
